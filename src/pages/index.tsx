ab07e42806b785d175e539952ff603a8a68fea80
João Vinícius Guerber de Souza <joaoviniciusgs@gmail.com>
2024-07-04 15:07:48 -0300

    implementado layout da home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,53 @@
 import { styled } from "../styles";
+import { HomeContainer, Product } from "../styles/pages/home";
+import { useKeenSlider } from "keen-slider/react";
 
-const Button = styled("button", {
-  background: "$gray100",
-  borderRadius: 8,
-  border: 0,
-  span: {
-    fontWeight: "bold"
-  }
-})
+import Camsieta1 from "../assets/camisetas/1.png";
+import Camsieta2 from "../assets/camisetas/2.png";
+import Camsieta3 from "../assets/camisetas/3.png";
+import Image from "next/image";
+
+import "keen-slider/keen-slider.min.css";
 
 export default function Home() {
+
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48
+    }
+  });
+
   return (
-    <Button><span>test</span>Enviar</Button>
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      <Product className="keen-slider__slide">
+        <Image src={Camsieta1} width={520} height={480} alt=""></Image>
+        <footer>
+          <strong>camiseta x</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product >
+      <Product className="keen-slider__slide">
+        <Image src={Camsieta1} width={520} height={480} alt=""></Image>
+        <footer>
+          <strong>camiseta x</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product className="keen-slider__slide">
+        <Image src={Camsieta1} width={520} height={480} alt=""></Image>
+        <footer>
+          <strong>camiseta x</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product className="keen-slider__slide">
+        <Image src={Camsieta1} width={520} height={480} alt=""></Image>
+        <footer>
+          <strong>camiseta x</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
   );
 }
