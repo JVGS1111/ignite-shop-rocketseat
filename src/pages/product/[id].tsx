2eeba54dd767c23c23034128bb4f3a33c40843ee
João Vinícius Guerber de Souza <joaoviniciusgs@gmail.com>
2024-07-05 11:27:44 -0300

    estrutura da pagina de produto

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,8 +1,22 @@
+import { ImageContainer, ProductContainer, ProductDetails } from "@/src/styles/pages/products";
 import { useRouter } from "next/router"
 
 export default function Products() {
     const { query } = useRouter();
     return (
-        <h1>Products: {JSON.stringify(query)}</h1>
+        <ProductContainer>
+            <ImageContainer>
+
+            </ImageContainer>
+
+            <ProductDetails>
+                <h1>Camisa X</h1>
+                <span>R$ 79,90</span>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam at obcaecati itaque suscipit sint nobis enim dolore consectetur eligendi deserunt. Illum quod repudiandae ducimus consequuntur culpa numquam facere modi tempora!</p>
+                <button>
+                    Comprar agora
+                </button>
+            </ProductDetails>
+        </ProductContainer>
     )
 }
